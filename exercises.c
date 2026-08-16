7b8e137735fee78f622ab818f7d4d0657fef2b3a
Your Name <you@example.com>
2026-08-16 03:33:47 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -139,14 +139,14 @@ int parentesisBalanceados(char *cadena)
    Stack *pilaParentesis = create_stack();
    int i = 0;
 
-   while (cadena[i] != '\n')
+   while (cadena[i] != '\0')
    {
       char parentesis = cadena[i];
       if (parentesis == '(')
       {
          push(pilaParentesis, parentesis);
       }
-      if(parentesis == ')')
+      if (parentesis == ')')
       {
          pop(pilaParentesis);
       }
